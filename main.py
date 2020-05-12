07a95437f64ece4b6eda525fefbb647024e8df44
lpdoyle <lpdoyle@ucsd.edu>
2020-05-11 17:56:47 -0700

    webpage features

--- main.py
+++ main.py
@@ -8,7 +8,28 @@ app = Flask(__name__)
 
 @app.route("/")  # endpoint "/" (home page)
 def home():                    
-    return render_template("home.html")
+    return render_template("home.html", data=[
+                                        {'name':'Option 1'},
+                                        {'name':'Option 2'}, 
+                                        {'name':'Option 3'}, 
+                                        {'name':'etc.'}
+                                        ])
+
+@app.route("/result", methods=['GET', 'POST'])
+def result():
+    data = []
+    error = None
+    
+    #select = request.form.get('comp_select')
+
+    #resp = query_api(select)
+    #pp(resp)
+    #if resp:
+        #data.append(resp)
+        #if len(data) != 2:
+            #error = 'Bad Response from API'
+    
+    return render_template('result.html', data=data, error=error)
 
 @app.route("/about")
 def about():
@@ -17,10 +38,7 @@ def about():
 @app.route("/example")
 def example():
     return "another example page :)"
-
-#@app.route("/<name>")              # at the end point /<name>
-#def hello_name(name):              # call method hello_name
-#    return "Hello "+ name           
+     
 
 if __name__ == "__main__":
     app.run(debug=True)     
