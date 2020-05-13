8d1a70b04dc0138b031f8d8a28033af3e35d6b91
pace1997 <44416381+pace1997@users.noreply.github.com>
2020-05-12 19:28:09 -0700

    v1

--- run.py
+++ run.py
@@ -3,40 +3,50 @@
 import sys
 import json
 import shutil
+import time
+import pickle
 
 sys.path.insert(0, 'src') # add library code to path
+from etl import get_data
 
-from get_data import get_listings, clean_listings
-# from model import driver 
-# etc.
+DATA_PARAMS = 'config/data-params.json'
+TEST_PARAMS = 'config/test-params.json'
+ENV = 'config/env.json'
 
-GET_DATA_PARAMS = 'config/get-data-params.json'
-# MODEL_PARAMS = 'config/model-params.json'
-# etc.
 
 def load_params(fp):
     with open(fp) as fh:
         param = json.load(fh)
-    return param
 
+    return param
 
 def main(targets):
 
+    # to time the function
+    start_time = time.time()
+    # make the clean target
     if 'clean' in targets:
-        shutil.rmtree('data/raw', ignore_errors=True)
+        shutil.rmtree('data/temp',ignore_errors=True)
+        shutil.rmtree('data/out',ignore_errors=True)
+        shutil.rmtree('data/raw',ignore_errors=True)
 
-    # data ingestion 
+    # make the data target
     if 'data' in targets:
-        cfg = load_params(GET_DATA_PARAMS)
-        get_listings(**cfg)
+        cfg = load_params(DATA_PARAMS)
 
-    #if 'model' in targets:
-    #    cfg = load_params(MODEL_PARAMS)
-    #    driver(**cfg)
+    # make the test target
+    if 'test-project' in targets:
+        cfg = load_params(TEST_PARAMS)
 
+    env = load_params(ENV)
+    
+    df, target = get_data(**cfg)
+    total = pd.concat([df, target])
+
+    print('Finished in: {} seconds'.format(time.time() - start_time))
     return
+
 
 if __name__ == '__main__':
     targets = sys.argv[1:]
     main(targets)
-    
