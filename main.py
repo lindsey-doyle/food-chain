6af1ba9879aa9629d8e73936577c0fcac642347e
lpdoyle <lpdoyle@ucsd.edu>
2020-06-02 22:42:34 -0700

    remove invoke app

--- main.py
+++ main.py
@@ -142,5 +142,5 @@ def example():
     return "another example page :)"
      
 
-if __name__ == "__main__":
-    app.run(debug=True)     
+#if __name__ == "__main__":
+    #app.run(debug=True)     
